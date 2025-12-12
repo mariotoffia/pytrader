44338fcd96d0d53b124ecc23323b3935da6c7e4c
Mario Toffia <mario.toffia@gmail.com>
2025-12-12 17:10:00 +0100

    feat: Add support for additional HTTP methods in CORS configuration

--- services/gateway/src/index.ts
+++ services/gateway/src/index.ts
@@ -109,6 +109,7 @@ class GatewayService {
       await this.fastify.register(cors, {
         origin: true, // Allow all origins in development
         credentials: true,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
       });
 
       // Register WebSocket support
